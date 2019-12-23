d2205f2efd6c88065b7fafb20ec4ac87aad7353e
LauraBeatris <laurabeatriserafim@gmail.com>
2019-12-23 10:39:41 -0300

    first setup

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <Text> Begging </Text>
+      </SafeAreaView>
+    </>
+  );
+};
+
+export default App;
